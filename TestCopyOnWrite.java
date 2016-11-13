2bc850f8828d8bf77053127f68b1f01fde9c532a
JeremyJiWZ <mr.ji@icloud.com>
2016-11-13 21:15:44 +0800

    测试CopyOnWriteList对于多线程的作用

---
+++ TestCopyOnWrite.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+ 
+class AddThread implements Runnable {
+    private List<Double> list;
+ 
+    public AddThread(List<Double> list) {
+        this.list = list;
+    }
+ 
+    @Override
+    public void run() {
+        for(int i = 0; i < 10; ++i) {
+        		double t = Math.random();
+            list.add(t);
+            System.out.println(t);
+            System.out.println(list.size());
+        }
+    }
+}
+ 
+public class TestCopyOnWrite {
+    private static final int THREAD_POOL_SIZE = 2;
+ 
+    public static void main(String[] args) {
+        List<Double> list = new CopyOnWriteArrayList<>();
+        ExecutorService es = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        es.execute(new AddThread(list));
+        es.execute(new AddThread(list));
+        es.shutdown();
+    }
+}
